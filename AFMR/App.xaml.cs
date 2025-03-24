4183f96f21b5149f9281939121d4a3ae8e55f57a
Firzus <92395888+Firzus@users.noreply.github.com>
2025-03-24 02:42:44 +0100

    Initialize MAUI application structure with main components and resources

---
+++ AFMR/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AFMR;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ AFMR/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AFMR;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
